19d5d9aa4a0a02e662edbacf3de0364a9441aa4d
HuanyuLee <hadoop_2020@163.com>
2022-10-05 15:37:13 +0800

    Stream流操作初体验

--- stream/src/class01_test/Code01_FindName.java
+++ stream/src/class01_test/Code01_FindName.java
@@ -5,12 +5,14 @@ import java.util.Arrays;
 import java.util.List;
 
 /**
+ * <a href="https://docs.oracle.com/javase/8/docs/api/index.html">JDK8 Stream API</a>
  * @author HuanyuLee
  * @date 2022/10/5
  */
 public class Code01_FindName {
-
-
+    // 查找集合中姓张的人
+    // 再筛选出张姓中名字有三个字的人
+    // 最后打印出来
     public static void main(String[] args) {
         List<String> names = new ArrayList<>(
                 Arrays.asList("张无忌", "周芷若", "赵敏", "张强", "张三丰"));
@@ -25,6 +27,5 @@ public class Code01_FindName {
                 .filter(name -> name.startsWith("张"))
                 .filter(name -> name.length() == 3)
                 .forEach(System.out::println);
-
     }
 }
