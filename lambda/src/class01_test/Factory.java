8c47d0223619bb17872429ea8042322f2c924099
HuanyuLee <hadoop_2020@163.com>
2022-10-07 23:45:21 +0800

    使用Lambda表达式的前提

--- lambda/src/class01_test/Factory.java
+++ lambda/src/class01_test/Factory.java
@@ -1,9 +1,11 @@
 package class01_test;
 
 /**
+ * 使用lambda表达式的前提：必须有一个函数式的、且仅有一个抽象方法的接口
  * @author HuanyuLee
  * @date 2022/10/6
  */
 public interface Factory {
     Object getObject();
+    // Object f();
 }
